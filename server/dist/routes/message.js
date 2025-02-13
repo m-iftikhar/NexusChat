b9cce715d07ebab397d65547cbe2673531291ec3
m-iftikhar <iftikharrj2001@gmail.com>
2025-02-13 10:07:14 +0500

    create msg route done

--- server/dist/routes/message.js
+++ server/dist/routes/message.js
@@ -3,10 +3,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dbclient_1 = __importDefault(require("../config/dbclient"));
 const express_1 = __importDefault(require("express"));
+const user_1 = require("../middleware/user");
 const messageRoute = (0, express_1.default)();
-messageRoute.post("/create", async (req, res) => {
+messageRoute.post("/create", user_1.auth, async (req, res) => {
     try {
+        const { receiverId, content } = req.body;
+        const userId = req?.user?.id;
+        if (!userId) {
+            return res.status(401).json({ error: "unauthorized" });
+        }
+        if (!receiverId || !content) {
+            return res.status(401).json({ error: "receiver id and conetnt are required" });
+        }
+        const message = await dbclient_1.default.message.create({
+            data: {
+                content: content,
+                senderId: userId,
+                receiverId: receiverId
+            }
+        });
+        res.status(200).json(message);
     }
     catch (error) {
         res.status(500).json({ message: error?.message });
--- server/src/routes/message.ts
+++ server/src/routes/message.ts
@@ -5,9 +5,24 @@ import { auth } from "../middleware/user";
 
 const messageRoute = Router(); 
 
-messageRoute.post("/create",async(req:any,res:any)=>{
+messageRoute.post("/create",auth,async(req:any,res:any)=>{
     try {
-        
+        const {receiverId , content}=req.body;
+        const userId=req?.user?.id;
+        if(!userId){
+            return res.status(401).json({error:"unauthorized"})
+        }
+        if(!receiverId || !content){
+            return res.status(401).json({error:"receiver id and conetnt are required"})
+        }
+        const message= await prisma.message.create({
+            data:{
+                content:content as string,
+                senderId:userId as number,
+                receiverId:receiverId as number
+            }
+        });
+        res.status(200).json(message)
     } catch (error:any) {
         res.status(500).json({message:error?.message})
     }
