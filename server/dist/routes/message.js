8dbe71e2b38b05e1d3a6372d7fa21770f999e187
m-iftikhar <iftikharrj2001@gmail.com>
2025-02-13 10:44:20 +0500

    get messages route done

--- server/dist/routes/message.js
+++ server/dist/routes/message.js
@@ -7,6 +7,7 @@ const dbclient_1 = __importDefault(require("../config/dbclient"));
 const express_1 = __importDefault(require("express"));
 const user_1 = require("../middleware/user");
 const messageRoute = (0, express_1.default)();
+// create message
 messageRoute.post("/create", user_1.auth, async (req, res) => {
     try {
         const { receiverId, content } = req.body;
@@ -25,6 +26,32 @@ messageRoute.post("/create", user_1.auth, async (req, res) => {
             }
         });
         res.status(200).json(message);
+    }
+    catch (error) {
+        res.status(500).json({ message: error?.message });
+    }
+});
+// get message
+messageRoute.get("/", user_1.auth, async (req, res) => {
+    try {
+        const { senderId } = req.query;
+        const userId = req?.user?.id;
+        if (!userId) {
+            return res.status(401).json({ error: "Unauthorized" });
+        }
+        const message = await dbclient_1.default.message.findMany({
+            where: {
+                OR: [
+                    { senderId: Number(userId), receiverId: Number(senderId) },
+                    { senderId: Number(senderId), receiverId: Number(userId) }
+                ]
+            },
+            include: {
+                sender: true, // Relation "SentMessages"
+                receiver: true // Relation "ReceivedMessages"
+            }
+        });
+        res.json(message);
     }
     catch (error) {
         res.status(500).json({ message: error?.message });
--- server/src/routes/message.ts
+++ server/src/routes/message.ts
@@ -4,7 +4,8 @@ import { auth } from "../middleware/user";
 
 
 const messageRoute = Router(); 
-
+  
+// create message
 messageRoute.post("/create",auth,async(req:any,res:any)=>{
     try {
         const {receiverId , content}=req.body;
@@ -23,6 +24,35 @@ messageRoute.post("/create",auth,async(req:any,res:any)=>{
             }
         });
         res.status(200).json(message)
+    } catch (error:any) {
+        res.status(500).json({message:error?.message})
+    }
+})
+
+
+// get message
+
+messageRoute.get("/",auth,async(req:any,res:any)=>{
+    try {
+        const {senderId}=req.query;
+        const userId=req?.user?.id;
+        if(!userId){
+            return res.status(401).json({error:"Unauthorized"});
+        }
+        const message= await prisma.message.findMany({
+            where:{
+                OR:[
+                    {senderId:Number(userId),receiverId:Number(senderId)},
+                    {senderId:Number(senderId),receiverId:Number(userId)}
+                ]  
+            },
+            include: {
+                sender: true,  // Relation "SentMessages"
+                receiver: true // Relation "ReceivedMessages"
+            }
+        });
+        res.json(message);
+        
     } catch (error:any) {
         res.status(500).json({message:error?.message})
     }
